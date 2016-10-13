88863d277cd9fb7a4386748772c7b98b36dc7512
zhangwei <505659671@qq.com>
2016-10-13 14:07:17 +0800

    bean继承知识点的分支，不要merge

--- src/com/springtutorial/HelloWorld.java
+++ src/com/springtutorial/HelloWorld.java
@@ -1,16 +1,25 @@
 package com.springtutorial;
 
 public class HelloWorld {
-	private String message;
+	private String message1;
+	private String message2;
 	
-	public void getMessage() {
-		System.out.println("your message:" + message);
+	public void getMessage1() {
+		System.out.println("your message1:" + message1);
 	}
 	
-	public void setMessage(String message) {
-		this.message = message;
+	public void setMessage1(String message) {
+		this.message1 = message;
 	}
 	
+	public void getMessage2() {
+		System.out.println("your message2:" + message2);
+	}
+	
+	public void setMessage2(String message) {
+		this.message2 = message;
+	}
+	/*
 	public void init() {
 		System.out.println("this is init method invoked");
 	}
@@ -18,4 +27,5 @@ public class HelloWorld {
 	public void destroy() {
 		System.out.println("this is destroy method invoked");
 	}
+	*/
 }
--- src/com/springtutorial/MainApp.java
+++ src/com/springtutorial/MainApp.java
@@ -9,18 +9,27 @@ public class MainApp {
 	public static void main(String[] args) {
 
 		// 获得应用的上下文，处理创建并初始化所有的对象
-//		ApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
+		ApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
 
 		// 使用已创建的上下文的 getBean() 方法来获得所需的 bean，
 		// 这个方法使用 bean 的 ID 返回一个最终可以转换为实际对象的通用对象
 		// 这里的getBean的参数对应的是Beans.xml中的id=""，如果不一致，报错。
 		// 输出的值是id=""对应的value值。
 
-		AbstractApplicationContext apContext = new ClassPathXmlApplicationContext("Beans.xml");
-		HelloWorld objA = (HelloWorld) apContext.getBean("helloworld");
-		objA.setMessage("Hello, I am objA");
-		objA.getMessage();
-		apContext.registerShutdownHook();
+//		AbstractApplicationContext apContext = new ClassPathXmlApplicationContext("Beans.xml");
+//		HelloWorld objA = (HelloWorld) apContext.getBean("helloworld");
+//		objA.setMessage1("Hello, I am objA");
+//		objA.getMessage1();
+//		apContext.registerShutdownHook();
+		
+		HelloWorld objA = (HelloWorld) context.getBean("helloworld");
+		objA.getMessage1();
+		objA.getMessage2();
+		
+		HelloChina objB = (HelloChina) context.getBean("helloChina");
+		objB.getMessage1();
+		objB.getMessage2();
+		objB.getMessage3();
 		
 	}
 
