886dc26664affa729ed71687ed37e5a7f2c87b4d
zhangwei <505659671@qq.com>
2016-10-13 13:42:47 +0800

    在InitHelloWorld类中加了两行注释，方便以后阅读

--- src/com/springtutorial/InitHelloWorld.java
+++ src/com/springtutorial/InitHelloWorld.java
@@ -4,7 +4,16 @@ import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.config.BeanPostProcessor;
 
 public class InitHelloWorld implements BeanPostProcessor {
-
+	
+	//这是初始化init-method方法执行前执行的方法
+	@Override
+	public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
+		
+		System.out.println("BeforeInitialization : " + beanName);
+		return bean;
+	}
+	
+	//这是init-method方法执行后执行的方法
 	@Override
 	public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
 		 
@@ -12,11 +21,6 @@ public class InitHelloWorld implements BeanPostProcessor {
 		return bean;
 	}
 
-	@Override
-	public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
-		
-		System.out.println("BeforeInitialization : " + beanName);
-		return bean;
-	}
+	
 	
 }
