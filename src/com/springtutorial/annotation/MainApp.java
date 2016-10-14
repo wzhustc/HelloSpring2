7f6f6ff533f3c591710b3201fe584380630e0bb1
zhangwei <505659671@qq.com>
2016-10-14 09:54:35 +0800

    添加profile.java文件，学习@Qualifier关键字

--- src/com/springtutorial/annotation/MainApp.java
+++ src/com/springtutorial/annotation/MainApp.java
@@ -7,12 +7,18 @@ public class MainApp {
 	public static void main(String[] args) {
 		ApplicationContext con = new ClassPathXmlApplicationContext("Beans3.xml");
 		
-		Student st = (Student)con.getBean("student");
+		
+		Student st = (Student)con.getBean("student1");
 		System.out.println(st.getAge());
 		System.out.println(st.getName());
 		
 		System.out.println("------------------");
 		TextEditor te = (TextEditor) con.getBean("textEditor");
 		te.spellCheck();
+		
+		System.out.println("------------------");
+		Profile profile = (Profile) con.getBean("profile");
+		profile.printAge();
+		profile.printName();
 	}
 }
---
+++ src/com/springtutorial/annotation/Profile.java
@@ -0,0 +1,19 @@
+package com.springtutorial.annotation;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+
+public class Profile {
+	
+	@Autowired
+	@Qualifier("student2")
+	private Student student;
+	
+	public void printAge() {
+		System.out.println("age : " + student.getAge());
+	}
+	
+	public void printName() {
+		System.out.println("name : " + student.getName());
+	}
+}
