a150d1ba3625528f0563a184eb9eb074b4a9ed34
zhangwei <505659671@qq.com>
2016-10-22 13:21:21 +0800

    修改命名

--- src/com/springtutorial/jdbc/StudentDAOImpl.java
+++ src/com/springtutorial/jdbc/StudentDAOImpl.java
@@ -8,43 +8,43 @@ import org.springframework.jdbc.core.JdbcTemplate;
 public class StudentDAOImpl implements StudentDAO {
 
 	private DataSource dataSource;
-	private JdbcTemplate jdbcTemplateObject;
+	private JdbcTemplate jdbcTemplate;
 
 	public void setDataSource(DataSource dataSource) {
 		this.dataSource = dataSource;
-		this.jdbcTemplateObject = new JdbcTemplate(dataSource);
+		this.jdbcTemplate = new JdbcTemplate(dataSource);
 	}
 
 	public void create(String name, Integer age) {
 		String SQL = "insert into Student (name, age) values (?, ?)";
 
-		jdbcTemplateObject.update(SQL, name, age);
+		jdbcTemplate.update(SQL, name, age);
 		System.out.println("Created Record Name = " + name + " Age = " + age);
 		return;
 	}
 
 	public Student getStudent(Integer id) {
 		String SQL = "select * from Student where id = ?";
-		Student student = jdbcTemplateObject.queryForObject(SQL, new Object[] { id }, new StudentMapper());
+		Student student = jdbcTemplate.queryForObject(SQL, new Object[] { id }, new StudentMapper());
 		return student;
 	}
 
 	public List<Student> listStudents() {
 		String SQL = "select * from Student";
-		List<Student> students = jdbcTemplateObject.query(SQL, new StudentMapper());
+		List<Student> students = jdbcTemplate.query(SQL, new StudentMapper());
 		return students;
 	}
 
 	public void delete(Integer id) {
 		String SQL = "delete from Student where id = ?";
-		jdbcTemplateObject.update(SQL, id);
+		jdbcTemplate.update(SQL, id);
 		System.out.println("Deleted Record with ID = " + id);
 		return;
 	}
 
 	public void update(Integer id, Integer age) {
 		String SQL = "update Student set age = ? where id = ?";
-		jdbcTemplateObject.update(SQL, age, id);
+		jdbcTemplate.update(SQL, age, id);
 		System.out.println("Updated Record with ID = " + id);
 		return;
 	}
